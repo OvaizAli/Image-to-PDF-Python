70098f2f95c6d71a5911da3da4c35723da846107
OvaizAli <ovaizali123@gmail.com>
2021-01-28 17:53:13 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+# Method 1
+from PIL import Image
+path = r'C:\..\Image-to-PDF-Python\Sample-Image.jpg'
+pdf = Image.open(path)
+pdf.save(r'C:\..\Sample-Image.pdf')
+
+# Method 2
 import img2pdf
 
-with open(r'C:\Users\Ovaiz Ali\Desktop\Development\Teach-Kolachi\Image-to-PDF-Python\Sample-Image.pdf', 'wb') as f:
-    f.write(img2pdf.convert(r'C:\Users\Ovaiz Ali\Desktop\Development\Teach-Kolachi\Image-to-PDF-Python\Sample-Image.jpg'))
+with open(r'C:\..\Sample-Image.pdf', 'wb') as f:
+    f.write(img2pdf.convert(r'C:\..\Image-to-PDF-Python\Sample-Image.jpg'))
